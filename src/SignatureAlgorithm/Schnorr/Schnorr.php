edfce1fb6eb323552891ddd290066ee126993be6
Andrew Curtis <agc.curtis@gmail.com>
2022-10-18 14:11:26 -0400

    WIP Changes to allow Schnorr Verification to work with Jose JWT and JWS

--- src/SignatureAlgorithm/Schnorr/Schnorr.php
+++ src/SignatureAlgorithm/Schnorr/Schnorr.php
@@ -7,6 +7,7 @@ namespace Jose\Component\Signature\Algorithm;
 use Exception;
 use InvalidArgumentException;
 use Jose\Component\Core\JWK;
+use Jose\Component\Signature\Serializer\CompactSerializer;
 use Mdanter\Ecc\Crypto\Signature\SchnorrSignature;
 use ParagonIE\ConstantTime\Base64UrlSafe;
 
@@ -26,6 +27,7 @@ abstract class Schnorr implements SignatureAlgorithm
         }
 
         try {
+            // TODO: verify if works as below or needs to be changed to $key->get('d');
             $d = bin2hex(Base64UrlSafe::decodeNoPadding($key->get('d')));
 
             $schnorrSig = (new SchnorrSignature())->sign($d, $input);
@@ -42,10 +44,16 @@ abstract class Schnorr implements SignatureAlgorithm
 
         $publicKey = $key->toPublic();
 
-        $publicKeyHex = bin2hex(Base64UrlSafe::decodeNoPadding($publicKey->get('x')));
+        $jwss = new CompactSerializer();
+
+        $jws = $jwss->unserialize($input . '.' . $signature);
+
+        $message = json_decode($jws->getPayload());
+
+        $publicKeyHex = $publicKey->get('x');
 
         try {
-            return (new SchnorrSignature())->verify($publicKeyHex, $signature, $input);
+            return (new SchnorrSignature())->verify($publicKeyHex, $signature, $message);
         } catch (\Exception $e) {
             throw new Exception($e->getMessage());
         }
